b0c72621d0171dadb5e5147cce04eabba472650c
Jorge <JorgePinho@sh_jorgepinho.Shortcut.local>
2012-12-10 15:40:47 +0000

    fix #3 missing header file in previous commit

--- 1.7/xcoder_b2b/xcoder_b2b_load.h
+++ 1.7/xcoder_b2b/xcoder_b2b_load.h
@@ -4,8 +4,12 @@
 int add_b2b_callID(char * orig_callID, char * b2b_callID, char * b2b_server_callID, char * b2b_key,char * original_tag,char * b2b_generated_tag);
 typedef int (*add_b2b_callID_f)(char * orig_callID, char * b2b_callID, char * b2b_server_callID,char * b2b_key,char * original_tag,char * b2b_generated_tag);
 
+int free_xcoder_resources(char * callID);
+typedef int (*free_xcoder_resources_f)(char * callID);
+
 struct xcoder_binds {
         add_b2b_callID_f add_b2b_callID;
+        free_xcoder_resources_f free_xcoder_resources;
 };
 
 typedef int(*load_xcoder_f)( struct xcoder_binds *xcoder );
